acc260d3f1c942fb96f13f7828c971e758e6e3a1
yevheniimoskalenko <testjokerqwerty@gmail.com>
2021-01-17 20:25:25 +0200

    return jwt token

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,3 +1,23 @@
-export default (req, res) => {
-  return res.json(req.body);
+import User from '../model/user.model.js';
+import jwt from 'jsonwebtoken';
+import { compareSync } from 'bcrypt';
+import dotenv from 'dotenv';
+dotenv.config();
+
+export default async (req, res) => {
+  const { email, password } = req.body;
+  const candiadate = await new User({ email, password }).findById();
+  const isCorrect = compareSync(password, candiadate[0].password);
+  if (candiadate[0].email === email && isCorrect) {
+    const token = jwt.sign(
+      {
+        email: candiadate[0].email,
+        id: candiadate[0].id,
+      },
+      process.env.secret,
+      { expiresIn: 60 * 24 * 3600 }
+    );
+
+    return res.json({ token });
+  }
 };
