9e906a6d51e853c372639fc86de542506d1398c9
yevheniimoskalenko <testjokerqwerty@gmail.com>
2021-01-17 20:23:55 +0200

    reg finall

--- src/controllers/reg.controller.js
+++ src/controllers/reg.controller.js
@@ -1,6 +1,18 @@
 import User from '../model/user.model.js';
-
-export default (req, res) => {
-  new User(req.body).save();
-  return res.json(req.body);
+import { hashSync, genSaltSync } from 'bcrypt';
+export default async (req, res) => {
+  const { email, password } = req.body;
+  if (email === '') return res.json('Будь-ласка напишіть електроний адрес');
+  const salt = genSaltSync(10);
+  const candiadat = await new User({
+    email,
+    password: hashSync(password, salt),
+  });
+  const users = await candiadat.findById().then((result) => result.length);
+  if (users === 0) {
+    await candiadat.save();
+    return res.json('Успішно зареєстрований');
+  } else {
+    return res.json('Користувач уже був зареєстрований раніше');
+  }
 };
